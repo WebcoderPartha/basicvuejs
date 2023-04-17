81d62fb417b39593416bdb154b9e4af5ed604bac
WebcoderPartha <parthadev76@gmail.com>
2023-04-17 19:38:06 +0600

    Vue js data function

---
+++ vuejs/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div>
+    <h2>{{ title}}</h2>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      title: 'Welcome to Bangladesh'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
